740edc5b14c1e636f0541847efff7f7638dd063c
Kevin Stear <kevinstear@Kevins-MacBook-Pro.local>
2016-11-18 12:20:23 -0700

    Added IPv4 and Domain shell scripts for OTX enrichment

---
+++ domain_pdns.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "...starting OTX indicator pulls!"
+while read p; do
+	echo "${p}"
+	curl -H "X-OTX-API-KEY: 93035a064e8463c43f2804937e8aa45be81ce97f6987c1dac006d50a838d23a5" --fail https://otx.alienvault.com:443/api/v1/indicators/domain/${p}/passive_dns >> ${p}_pDNS.txt
+done < domain_input.txt

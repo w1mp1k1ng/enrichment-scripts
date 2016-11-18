1dbf15d5ea4e23aeda05731543daea361c307357
w1mp1k1ng <kevin.stear@rsa.com>
2016-11-18 12:21:10 -0700

    Delete indicators_enrich.sh

--- indicators_enrich.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "...starting OTX indicator pulls!"
-while read p; do
-	echo "${p}"
-	curl -H "X-OTX-API-KEY: 93035a064e8463c43f2804937e8aa45be81ce97f6987c1dac006d50a838d23a5" --fail https://otx.alienvault.com:443/api/v1/indicators/IPv4/${p}/reputation >> output.txt
-done < input.txt

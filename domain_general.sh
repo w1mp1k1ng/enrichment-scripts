b73149a9c7d9db21ceed05dc3ce5446726ea6f13
Kevin Stear <kevinstear@Kevins-MacBook-Pro.local>
2016-11-21 10:44:48 -0700

    fixed output file naming

--- domain_general.sh
+++ domain_general.sh
@@ -3,5 +3,5 @@
 echo "...starting OTX indicator pulls!"
 while read p; do
 	echo "${p}"
-	curl -H "X-OTX-API-KEY: 93035a064e8463c43f2804937e8aa45be81ce97f6987c1dac006d50a838d23a5" --fail https://otx.alienvault.com:443/api/v1/indicators/IPv4/${p}/general | grep -Po '(?<="pulses": ")[^"]*' >> ${p}_pulses.txt
+	curl -H "X-OTX-API-KEY: 93035a064e8463c43f2804937e8aa45be81ce97f6987c1dac006d50a838d23a5" --fail https://otx.alienvault.com:443/api/v1/indicators/IPv4/${p}/general | grep -Po '(?<="pulses": ")[^"]*' >> ${p}_general.txt
 done < input.txt
